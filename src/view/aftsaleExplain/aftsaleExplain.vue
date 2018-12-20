dd709c384b64b314a687216b95467728e8e3fd4a
qq757622960 <qq757622960>
2018-12-20 14:05:02 +0800

    售后说明页面
    售后说明页面

--- src/view/aftsaleExplain/aftsaleExplain.vue
+++ src/view/aftsaleExplain/aftsaleExplain.vue
@@ -1,6 +1,27 @@
 <template>
     <sale htitle="售后说明">
-        售后说明
+        <div class="wrapper">
+            <div class="cont">
+                <ul class="list">
+                    <li class="item">
+                        <span>1.</span>
+                        因产品质量问题，自收到产品签收日起3个工作日内联系售后热线中心处理
+                    </li>
+                    <li class="item">
+                        <span>2.</span>
+                        对于产品的质量，以及产品错发、漏发、破损问题所产生的邮费，将由我司承担往返运费
+                    </li>
+                    <li class="item">
+                        <span>3.</span>
+                        退换品在购买时如为优惠装或附有赠品，须将已优惠或赠送的产品/物品一并退回
+                    </li>
+                    <li class="item">
+                        <span>4.</span>
+                        在商品无任何情况下，自实际收到商品之日起7日内，商品往返运费由客户承担
+                    </li>
+                </ul>
+            </div>
+        </div>
     </sale>
 </template>
 
@@ -27,15 +48,24 @@
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-    .m-wrapper
-        position: fixed
-        width 100%
-        top: 50px
-        bottom: 0
-        max-width 540px
-        min-width 320px
-        background-color #fff
-        .wrapper
-            height 100%
-            overflow hidden
+    .wrapper
+        min-height 100%
+        margin 0 10px 10px 10px
+        .list
+            .item
+                position relative
+                padding 18px 15px 18px 40px
+                margin-bottom 10px
+                background-color #fff
+                font-size 16px
+                color #666
+                border-radius 5px
+                span 
+                    font-size 20px
+                    color #666
+                    font-weight 700
+                    position absolute
+                    left 15px
+                    top 50%
+                    transform translate(0, -50%)
 </style>
